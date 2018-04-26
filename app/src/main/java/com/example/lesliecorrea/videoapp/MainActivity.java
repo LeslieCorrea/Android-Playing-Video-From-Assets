a90a14ca2fec2632b103f70de64433088b340f9f
LESLIE CORREA <correaleslie69@yahoo.com>
2018-04-26 16:30:35 +0530

    Added video files commit

--- app/src/main/java/com/example/lesliecorrea/videoapp/MainActivity.java
+++ app/src/main/java/com/example/lesliecorrea/videoapp/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.lesliecorrea.videoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVideoView = (VideoView) this.findViewById(R.id.SampleVideoView);
+        MediaController mediaController = new MediaController(this);
+        mVideoView.setMediaController(mediaController);
+        mVideoView.setVideoPath("content://com.example.lesliecorrea.videoapp/video.mp4");
+        mVideoView.start();
     }
 }
